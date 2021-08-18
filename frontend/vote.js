493f104878cd30410ae838e2f34aba8b6b6b707b
Loktev <businessmilkshake@gmail.com>
2021-08-18 13:25:50 +0300

    Finalized the necessary pages and prepared the project to be filled with content

---
+++ frontend/vote.js
@@ -0,0 +1,28 @@
+"use strict";
+
+function add_like(e) {
+    likescount++;
+    likes.innerHTML = likescount;
+    setTimeout(function (e) {
+        likescount = 0;
+        likes.innerHTML = likescount;
+
+    }, 2500)
+}
+
+function add_dislike(e) {
+    dislikescount++;
+    dislikes.innerHTML = dislikescount;
+}
+
+let likescount = 0;
+let dislikescount = 0;
+like.addEventListener('click', add_like);
+dislike.addEventListener('click', add_dislike);
+setTimeout(function (e) {
+    like.removeEventListener('click', add_like);
+    like.addEventListener('click', function (e) {
+        likescount -= 10;
+        likes.innerHTML = likescount;
+    })
+}, 30000)
