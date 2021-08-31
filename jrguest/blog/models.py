6311a5aedd495aaed77ba27d97446bc1f0f286e5
Loktev <businessmilkshake@gmail.com>
2021-08-31 13:55:14 +0300

    Added a database and changed the website to fit it

--- jrguest/blog/models.py
+++ jrguest/blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Posts(models.Model):
+    title = models.TextField()
+    description = models.TextField()
+    date = models.DateField()
--- jrguest/blog/views.py
+++ jrguest/blog/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+from blog.models import Posts
+
+
+def get_posts():
+    return Posts.objects.all()
 
 
 def index_page(request):
-    return render(request, 'index.html')
+    context = {
+        'posts': get_posts()
+    }
+    return render(request, 'index.html', context)
 
 
 def blog_page(request):
-    return render(request, 'blog.html')
+    context = {
+        'posts': get_posts()
+    }
+    return render(request, 'blog.html', context)
 
 
 def about_page(request):
